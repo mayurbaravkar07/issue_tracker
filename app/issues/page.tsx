7524a724d2ae9af693cbf47fa0a501cb4ad12ec6
Mayur Baravkar <mayurbaravkar24@gmail.com>
2024-04-07 17:24:12 +0530

    Shown Issue Details

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -8,7 +8,7 @@ import IssueActions from './issueActions'
 
 const IssuesPage = async () => {
     const issues = await prisma.issue.findMany();
-    await delay(2000);
+    await delay(1000);
     return (
         <div >
             <IssueActions />
@@ -23,7 +23,10 @@ const IssuesPage = async () => {
                 <Table.Body>
                     {issues.map(issue => (
                         <Table.Row key={issue.id}>
-                            <Table.Cell>{issue.title}
+                            <Table.Cell>
+                                <Link href={`/issues/${issue.id}`}>  {issue.title}</Link>
+
+
                                 <div className='block md:hidden'><IssueStatusBadge status={issue.status} /></div></Table.Cell>
                             <Table.Cell className='hidden md:table-cell'><IssueStatusBadge status={issue.status} /></Table.Cell>
                             <Table.Cell className='hidden md:table-cell'>{issue.createdAt.toDateString()}</Table.Cell>
