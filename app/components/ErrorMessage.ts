74a1cc18a44433a72c5db57a12b91597478e7d60
Mayur Baravkar <mayurbaravkar24@gmail.com>
2024-04-07 11:57:33 +0530

    Moved inline function onSumbit

---
+++ app/components/ErrorMessage.ts
@@ -0,0 +1,10 @@
+// import React, { PropsWithChildren } from 'react'
+// import { Text } from '@radix-ui/themes'
+
+// const ErrorMessage = ({ children }: PropsWithChildren) => {
+//     return (
+//         <Text></Text>
+//   );
+// }
+
+// export default ErrorMessage;
