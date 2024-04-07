f3d9f83db54457bf49dea9c26242951a96c96b8e
Mayur Baravkar <mayurbaravkar24@gmail.com>
2024-04-07 15:57:33 +0530

    Built Issue Status BADGE

---
+++ app/components/IssueStatusBadge.tsx
@@ -0,0 +1,18 @@
+import { Status, Issue } from '@prisma/client'
+import React from 'react'
+import { Badge } from '@radix-ui/themes'
+
+const statusMap: Record<Status, { label: string; color?: 'red' | 'violet' | 'green' }> = {
+    OPEN: { label: 'Open', color: 'red' },
+    IN_PROGRESS: { label: 'In Progress', color: 'violet' },
+    CLOSED: { label: 'Closed', color: 'green' }
+};
+
+const IssueStatusBadge = ({ status }: { status: Status }) => {
+
+    return (
+        <Badge color={statusMap[status].color}>{statusMap[status].label}</Badge>
+    );
+}
+
+export default IssueStatusBadge
