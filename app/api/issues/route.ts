1e48c2e97ef83438b4092120c97cbe3b0121c814
Mayur Baravkar <mayurbaravkar24@gmail.com>
2024-04-07 09:12:10 +0530

    Built API to create and Issue

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -23,6 +23,6 @@ export async function POST(request: NextRequest) {
         return NextResponse.json(newIssue, { status: 201 });
     } catch (error) {
         console.error("Error creating issue:", error);
-        return NextResponse.error("Internal Server Error", { status: 500 });
+        return NextResponse.error();
     }
 }
