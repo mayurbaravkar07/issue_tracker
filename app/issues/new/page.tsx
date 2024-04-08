e524a156d3f752fbe66fba35cd37b7289d976959
Mayur Baravkar <mayurbaravkar24@gmail.com>
2024-04-08 20:08:58 +0530

    Improved the Loading Experience of the new issue page

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,5 +1,16 @@
-import IssueForm from '../_components/issueForm'
+// import IssueForm from '../_components/issueForm'
+import dynamic from 'next/dynamic'
+import IssueFormSkeleton from './loading';
 
+
+const IssueForm = dynamic(
+    () => import('@/app/issues/_components/issueForm'),
+    {
+        ssr: false,
+        loading: () => <IssueFormSkeleton></IssueFormSkeleton>
+    }
+
+);
 const NewIssuePage = () => {
     return (
         <IssueForm></IssueForm>
