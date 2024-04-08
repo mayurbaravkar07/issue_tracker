dd8ee61d68180b0c52ad3a525393084b7227f928
Mayur Baravkar <mayurbaravkar24@gmail.com>
2024-04-08 20:08:58 +0530

    Improved the Loading Experience of the new issue page

--- app/issues/new/loading.tsx
+++ app/issues/new/loading.tsx
@@ -1,16 +1,5 @@
-import React from 'react'
-import { Box } from '@radix-ui/themes'
-import Skeleton from 'react-loading-skeleton'
-import 'react-loading-skeleton/dist/skeleton.css'
+import IssueFormSkeleton from '../_components/IssueFormSkeleton'
 
-const LoadingNewIssuePage = async () => {
 
-    return (
-        <Box className='max-w-xl'>
-            <Skeleton></Skeleton>
-            <Skeleton height="20rem"> </Skeleton>
-        </Box>
-    )
-}
 
-export default LoadingNewIssuePage
+export default IssueFormSkeleton
