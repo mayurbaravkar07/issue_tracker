48f9631768cbb99f82536b59fa47f6f1f504272a
Mayur Baravkar <mayurbaravkar24@gmail.com>
2024-04-09 13:18:42 +0530

    Secured the application endpoints

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -1,10 +1,16 @@
+import authOptions from "@/app/auth/authOptions";
 import { issueSchema } from "@/app/validationSchemas";
 import prisma from "@/prisma/client";
 import delay from "delay";
+import { getServerSession } from "next-auth";
 import { NextRequest, NextResponse } from "next/server";
 
 
 export async function PATCH(request: NextRequest, { params }: { params: { id: string } }) {
+    const session = await getServerSession(authOptions);
+    if (!session) {
+        return NextResponse.json({}, { status: 401 })
+    }
 
     const body = await request.json();
     const validation = issueSchema.safeParse(body);
@@ -33,7 +39,11 @@ export async function PATCH(request: NextRequest, { params }: { params: { id: st
 
 
 export async function DELETE(request: NextRequest, { params }: { params: { id: string } }) {
-    await delay(2000);
+    const session = await getServerSession(authOptions);
+    if (!session) {
+        return NextResponse.json({}, { status: 401 })
+    }
+
     const issue = await prisma.issue.findUnique({
         where: { id: parseInt(params.id) }
     });
--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -4,12 +4,15 @@ import { notFound } from 'next/navigation';
 import EditIssueButton from './EditIssueButton';
 import IssueDetails from './IssueDetails';
 import DeleteIssueButton from './DeleteIssueButton';
+import { getServerSession } from 'next-auth';
+import authOptions from '@/app/auth/authOptions';
 
 interface Props {
     params: { id: string }
 }
 const IssueDetailPage = async ({ params }: Props) => {
     // if (typeof params.id !== 'number') notFound();
+    const session = await getServerSession(authOptions);
 
     const issue = await prisma.issue.findUnique({
         where: { id: parseInt(params.id) }
@@ -25,14 +28,14 @@ const IssueDetailPage = async ({ params }: Props) => {
                 <IssueDetails issue={issue}></IssueDetails>
             </Box>
 
-            <Box>
+            {session && <Box>
                 <Flex direction='column' gap='3'>
                     <EditIssueButton issueId={issue.id}></EditIssueButton>
                     <DeleteIssueButton issueId={issue.id}></DeleteIssueButton>
                 </Flex>
 
             </Box>
-
+            }
 
         </Grid>
     )
