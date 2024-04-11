ba61e21af79c71f57bca9960c06923b486122684
Mayur Baravkar <mayurbaravkar24@gmail.com>
2024-04-11 17:43:34 +0530

    Fixed Fuiltering Issues

--- app/issues/list/IssueStatusFilter.tsx
+++ app/issues/list/IssueStatusFilter.tsx
@@ -1,7 +1,8 @@
 'use client'
 import { Status } from '@prisma/client';
 import { Select } from '@radix-ui/themes';
-import { useRouter } from 'next/navigation';
+import { useRouter, useSearchParams } from 'next/navigation';
+import { string } from 'zod';
 
 const statuses: { label: string, value?: Status }[] = [
     { label: 'All' },
@@ -11,9 +12,17 @@ const statuses: { label: string, value?: Status }[] = [
 ];
 const IssueStatusFilter = () => {
     const router = useRouter();
+    const searchParams = useSearchParams();
     return (
-        <Select.Root onValueChange={(status) => {
-            const query = status ? `?status=${status}` : '';
+        <Select.Root defaultValue={searchParams.get('status') || ''} onValueChange={(status) => {
+            const params = new URLSearchParams();
+            if (status)
+                params.append('status', status);
+
+            if (searchParams.get('orderBy'))
+                params.append('orderBy', searchParams.get('orderBy')!)
+
+            const query = params.size ? '?' + params.toString() : string;
             router.push('/issues/list' + query)
         }}>
             <Select.Trigger placeholder='Filter by Status...' />
